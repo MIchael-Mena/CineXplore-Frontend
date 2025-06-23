b528ba27ac849950d396af084dd73968a23df26e
Blisstar <joelferfox@gmail.com>
2025-06-22 21:48:27 -0300

    feat: Enhance MovieExpanded component to accept width prop and render Box with movie cover

--- src/screens/MovieExpanded.tsx
+++ src/screens/MovieExpanded.tsx
@@ -1,7 +1,19 @@
 import { Movie } from "@/schemas/movies";
+import { Box } from "@mui/material";
 
-export default function MovieExpanded({ movie } : { movie: Movie}) {
-    return {
-        
-    }
+export default function MovieExpanded({ movie, width } : { movie: Movie, width: string}) {
+    return (
+        <Box 
+        width={width}
+        display={'display'} 
+        flexDirection={'column'}
+        sx={{
+            backgroundImage: movie.coverUrl,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+        }}
+        >
+            
+        </Box>
+    )
 }
