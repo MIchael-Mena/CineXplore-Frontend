08a25739fcb740cb51b681fa6f4675887d620efa
Michael Mena <michael.mena2408@gmail.com>
2025-06-27 19:06:28 -0300

    update

--- src/config/snackbar.config.tsx
+++ src/config/snackbar.config.tsx
@@ -1,47 +1,18 @@
-import { SnackbarProvider } from 'notistack'; // Permite usar hook useSnackbar en cualquier componente
-import { closeSnackbar } from 'notistack';
-import { IconButton, styled } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import { MaterialDesignContent } from 'notistack';
+import { SnackbarProvider, closeSnackbar } from 'notistack';
+import React from 'react';
 
 type SnackbarConfigProps = {
   children: React.ReactNode;
 };
 
 export const SnackbarConfig: React.FC<SnackbarConfigProps> = ({ children }) => {
-  const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
-    '&.notistack-MuiContent': {
-      flexWrap: 'nowrap',
-      borderRadius: 5,
-    },
-  }));
-
   return (
-    <>
-      <SnackbarProvider
-        maxSnack={1}
-        Components={{
-          success: StyledMaterialDesignContent,
-          error: StyledMaterialDesignContent,
-          warning: StyledMaterialDesignContent,
-          info: StyledMaterialDesignContent,
-        }}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        action={(snackbarId) => (
-          <>
-            <IconButton
-              sx={{ display: 'inline-block' }}
-              onClick={() => {
-                return closeSnackbar(snackbarId);
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-          </>
-        )}
-      >
-        {children}
-      </SnackbarProvider>
-    </>
+    <SnackbarProvider
+      maxSnack={3}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      autoHideDuration={4000}
+    >
+      {children}
+    </SnackbarProvider>
   );
 };
